8f936fa2b97da9b26ca1ea752270e52d79e9313a
Isaac Nzekwe <curlyzik@gmail.com>
2024-01-02 22:55:49 +0100

    exported components

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 // import "./styles/tailwind.css"
 import Button from './components/Button';
+import Popover from './components/Popover';
+import RightDrawer from './components/RightDrawer';
 
-export { Button };
+export { Button, Popover, RightDrawer };
